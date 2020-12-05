61b4168adcfeb4c9b0f832a6ed8ca3ab38498079
rakesh <rksinghyadav1995@gmail.com>
2020-12-05 23:24:52 +0530

    project setup first time on git

---
+++ src/main/java/com/stackspaceit/SpringBootJdbcSecuritApplication.java
@@ -0,0 +1,13 @@
+package com.stackspaceit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJdbcSecuritApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJdbcSecuritApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stackspaceit/SpringBootJdbcSecuritApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stackspaceit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJdbcSecuritApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
